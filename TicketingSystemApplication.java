3f669b377004310cb2fc7b43e9a1736cce88989d
LAIYINAH <216263@student.upm.edu.my>
2024-06-24 22:12:44 +0800

    Add files via upload

---
+++ TicketingSystemApplication.java
@@ -0,0 +1,15 @@
+package com.csc3402.lab.ticketingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude ={SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class})
+public class TicketingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketingSystemApplication.class, args);
+    }
+
+}
